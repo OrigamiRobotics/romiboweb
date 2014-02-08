0afc7b586c1091b5d3badfdccaf226d8d1b7dd25
Daniel Brown <nartey_brown@yahoo.com>
2014-02-08 00:27:28 -0800

    added sign up form

--- app/assets/javascripts/home.js.coffee
+++ app/assets/javascripts/home.js.coffee
@@ -2,3 +2,4 @@ jQuery ->
   if gon? and gon.controller? and gon.action? and
       gon.controller == 'romiboweb_pages' and gon.action == 'home'
     $('#homeWrapper').height("#{screen.height - 200}px")
+    $('#signUpButton').css('margin-left', '200px')
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  helper_method :resource, :resource_name, :devise_mapping
 
   def set_gon
     gon.controller = params[:controller]
