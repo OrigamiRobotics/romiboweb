544b0cc988c19e456b00144875065ec54f5997a5
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2014-02-17 19:02:15 -0800

    ROM-11 - User signs in using email and password.

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature 'Sign in' do
+feature 'Sign in', auth: true do
 
   given(:new_user) {FactoryGirl.create :user}
   scenario 'registered user signing in with email and password' do
@@ -11,6 +11,17 @@ feature 'Sign in' do
     end
     click_on 'Sign in'
     expect(page).to have_title 'Dashboard'
+  end
+
+  scenario 'user signing with incorrect information' do
+    visit new_user_session_path
+    within('#sign_in_panel') do
+      fill_in 'Email', with: 'booboo'
+      fill_in 'Password', with: 'incorrect'
+    end
+    click_on 'Sign in'
+    #expect(page).to have_title 'Home'
+    expect(page).to have_content 'Invalid email or password'
   end
 
 end
