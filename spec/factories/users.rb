f6f348d3ab9741808feae12c5865b08c8cbd0909
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2014-02-17 19:41:51 -0800

    ROM-6 - User signs up using traditional form.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,10 +2,10 @@
 
 FactoryGirl.define do
   factory :user do
-    first_name 'Test'
-    last_name 'User'
-    email 'example1@example.com'
-    password 'please'
+    first_name Faker::Name.first_name
+    last_name Faker::Name.last_name
+    email Faker::Internet.email
+    password Faker::Internet.password
     #password_confirmation 'please'
     #confirmed_at Time.now
   end
