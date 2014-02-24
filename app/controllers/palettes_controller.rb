3e198bc4849c256bbbf97647862f2f60cab808ac
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2014-02-24 14:20:15 -0800

    ROM-90 - Palette create form

--- app/controllers/palettes_controller.rb
+++ app/controllers/palettes_controller.rb
@@ -1,14 +1,21 @@
 class PalettesController < ApplicationController
-  respond_to :html, :json
+  #respond_to :html, :json, :js
   before_filter :authenticate_user!
   def new
+    @palette = Palette.new
   end
 
   def create
     @palette =  current_user.palettes.build(palette_params)
-    @palette.save
     respond_to do |format|
-      format.json { render json: @palette }
+      if @palette.save
+        format.html {redirect_to romiboweb_pages_editor_path}
+        format.json { render json: @palette }
+        format.js #palettes/create.js.erb
+      else
+        format.html {redirect_to romiboweb_pages_editor_path}
+      end
+
     end
   end
 
@@ -27,8 +34,14 @@ class PalettesController < ApplicationController
   end
 
   def index
+    @title = 'Palette Editor'
+    @palette = Palette.new
     @palettes = Palette.where("owner_id = :id", { id: current_user.id })
-    respond_with @palettes
+    respond_to do |format|
+      format.html
+      format.json {render json: @palettes}
+    end
+    #respond_with @palettes
   end
 
   private
