1a57da14c06493045fc7df2dab7c6f36573fc733
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2014-03-08 03:07:26 -0800

    ROM-53 - email modal window and controllers

--- app/controllers/palettes/share_controller.rb
+++ app/controllers/palettes/share_controller.rb
@@ -1,8 +1,12 @@
 class Palettes::ShareController < ApplicationController
+  respond_to :html, :js
   def new
     @palette = Palette.find params[:palette_id]
+    respond_with @palette
   end
 
   def create
+    @palette = Palette.find params[:palette_id]
+    flash[:success] = 'Email has been sent. Thank you!'
   end
 end
