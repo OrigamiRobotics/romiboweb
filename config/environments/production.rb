c0fa1695197d05eb38899671fa261d6a08d45053
Daniel Brown <nartey_brown@yahoo.com>
2014-03-08 14:15:54 -0800

    --amend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,24 @@ Romiboweb::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'romiboweb.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  # change to false to prevent email from being sent during development
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      domain: 'gmail.com',
+      authentication: :login,
+      enable_starttls_auto: true,
+      user_name: "funcationer@gmail.com",
+      password:  "narteyfuncation",
+      :openssl_verify_mode  => 'none'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
