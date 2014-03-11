7bfbe20887cd765d111c0b776bb35e7b7946cd13
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2014-03-10 19:35:52 -0700

    ROM-53 - adding romibo gmail credentials

---
+++ config/initializers/dev_mail_interceptor.rb
@@ -0,0 +1,7 @@
+# Based on http://railscasts.com/episodes/206-action-mailer-in-rails-3
+class DevelopmentMailInterceptor
+  def self.delivering_email(message)
+    message.subject = "#{message.to} #{message.subject}"
+    message.to = ENV['SMTP_DEV_EMAIL']
+  end
+end
