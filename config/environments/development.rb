53291a929edc91668567843f785dd39b6914eacd
John Lee <john.lee@sv.cmu.edu>
2014-03-29 14:41:28 -0700

    Add content_for to Edit Password page

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,10 @@ Romiboweb::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  #config.action_mailer.delivery_method = :smtp
+  # change to false to prevent email from being sent during development
+  #config.action_mailer.perform_deliveries = true
+
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
