96a3ff36c358a3c942a3eb3cc7054ae5ce24e37e
Daniel Brown <nartey_brown@yahoo.com>
2014-04-01 02:14:10 -0700

    --amend

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,8 +63,8 @@ Romiboweb::Application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'romiboweb.elasticbeanstalk.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'create.romibo.com' }
 
   config.action_mailer.delivery_method = :smtp
   # change to false to prevent email from being sent during development
