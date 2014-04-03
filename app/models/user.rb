20f73813526a4a2b354074b73c31c55be16c4665
Abhinav Trivedi <abhinavtrivedi@gmail.com>
2014-04-03 15:33:30 -0700

    fixing broken user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -270,26 +270,26 @@ class User < ActiveRecord::Base
     end
   end
 
-  #def encrypt_id
-  #  data = "Very, very confidential data"
-  #
-  #  cipher = OpenSSL::Cipher::AES.new(128, :CBC)
-  #  cipher.encrypt
-  #
-  #  encryption = cipher.update(data) + cipher.final
-  #  encryption_key = cipher.random_key
-  #  encryption_iv  = cipher.random_iv
-  #end
-  #
-  #
-  #def decrypted_id
-  #  decipher = OpenSSL::Cipher::AES.new(128, :CBC)
-  #  decipher.decrypt
-  #  decipher.key = encryption_key
-  #  decipher.iv = encryption_iv
-  #
-  #  decipher.update(encryptd_id) + decipher.final
-  #end
+  def encrypt_id
+  data = "Very, very confidential data"
+  
+  cipher = OpenSSL::Cipher::AES.new(128, :CBC)
+  cipher.encrypt
+  
+  encryption = cipher.update(data) + cipher.final
+  encryption_key = cipher.random_key
+  encryption_iv  = cipher.random_iv
+  end
+  
+  
+  def decrypted_id
+  decipher = OpenSSL::Cipher::AES.new(128, :CBC)
+  decipher.decrypt
+  decipher.key = encryption_key
+  decipher.iv = encryption_iv
+  
+  decipher.update(encryptd_id) + decipher.final
+  end
 
   private
 
