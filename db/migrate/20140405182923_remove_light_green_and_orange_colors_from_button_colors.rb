afc63d81707759f47dc515b0e9efdcd6a2223022
Daniel Brown <nartey_brown@yahoo.com>
2014-04-05 12:01:24 -0700

    add migration for removing orange and light green from system button colors

---
+++ db/migrate/20140405182923_remove_light_green_and_orange_colors_from_button_colors.rb
@@ -0,0 +1,13 @@
+class RemoveLightGreenAndOrangeColorsFromButtonColors < ActiveRecord::Migration
+  def change
+    execute "UPDATE buttons
+            SET button_color_id=(select id FROM button_colors WHERE name='Green')
+            WHERE button_color_id=(select id FROM button_colors WHERE name='Light Green')"
+
+    execute "UPDATE buttons
+            SET button_color_id=(select id FROM button_colors WHERE name='Yellow')
+            WHERE button_color_id=(select id FROM button_colors WHERE name='Orange')"
+
+    execute "DELETE FROM button_colors WHERE name='Light Green' OR name='Orange'"
+  end
+end
